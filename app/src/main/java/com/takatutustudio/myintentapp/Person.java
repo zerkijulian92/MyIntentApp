0996220fcf694e9796a04c0c4d6a26bc8f2ff592
zerkijulian92 <zerkijuliandri92@gmail.com>
2019-08-20 14:35:12 +0700

    Menambahkan file Person.java dan getter and setter

---
+++ app/src/main/java/com/takatutustudio/myintentapp/Person.java
@@ -0,0 +1,40 @@
+package com.takatutustudio.myintentapp;
+
+public class Person {
+    private String name;
+    private  int age;
+    private String email;
+    private String city;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
